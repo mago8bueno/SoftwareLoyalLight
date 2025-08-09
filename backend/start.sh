2939c2b8b1669b681dec709c8e37ec1536063846
mago8bueno <122174547+mago8bueno@users.noreply.github.com>
2025-08-09 17:36:27 +0200

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Iniciando backend..."
-# Ya estás en /backend gracias a Railway, no hace falta cd
+if [ -d "backend" ]; then
+  cd backend
+fi
+
 exec python -m uvicorn app.main:app --host 0.0.0.0 --port "$PORT"
